58de2341682df7d509c9f036184cfd10d102dc5c
PTT-FULLSTACK <phamtrungtuyentm@gmail.com>
2024-07-31 19:22:31 +0700

    Fix interface

--- DAY-36/js/main.js
+++ DAY-36/js/main.js
@@ -4,11 +4,11 @@ F8.component("counter-app", {
         title: "Counter App",
     }),
     template: `
-    <h1>{{ title }}</h1>
-    <h2>Đã đếm: {{ count }} lần</h2>
-    <button v-on:click = "count--">-</button>
-    <button v-on:click = "count++">+</button>
-    <button v-on:dblclick ="title='Xin chào AE F8'">Change Title</button>
+        <h1>{{ title }}</h1>
+        <h2>Đã đếm: {{ count }} lần</h2>
+        <button v-on:click = "count--">-</button>
+        <button v-on:click = "count++">+</button>
+        <button v-on:dblclick ="title='Xin chào AE F8'">Change Title</button>
     `,
 });
 
