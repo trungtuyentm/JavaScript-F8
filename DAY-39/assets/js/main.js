e2f99f12cd142fb59dfcd2f7678bde9c4c0039a5
PTT-FULLSTACK <phamtrungtuyentm@gmail.com>
2024-08-14 22:28:19 +0700

    Submit session 39 asignment

---
+++ DAY-39/assets/js/main.js
@@ -0,0 +1,101 @@
+let pageNumber = 1;
+let pageSize = 8;
+let isPageLoad = true;
+const userContainer = document.querySelector(".user-container");
+const loadingEl = document.querySelector("#loading");
+
+const toggleLoading = (isLoading) => {
+    console.log(`${isLoading}`);
+    loadingEl.classList.toggle("show", isLoading);
+};
+
+const renderUser = (user) => {
+    let { picture, name, mutual } = user;
+    let htmlStr = `
+                    <div class="user">
+                        <div class="user-logo item">
+                            <img src="${picture}" />
+                        </div>
+                        <div class="user-name item">
+                            <a href="#!">${name}</a>
+                        </div>
+                        <div class="user-mutual item">${mutual}</div>
+                        <div class="user-messenger item">
+                            <a href="#!">
+                                <i class="fa-brands fa-facebook-messenger"></i
+                            ></a>
+                        </div>
+                        <a href="#!" class="menu item">
+                            <i class="fa-solid fa-ellipsis"></i>
+                        </a>
+                    </div>`;
+    userContainer.insertAdjacentHTML("beforeend", htmlStr);
+};
+
+async function getRandomUser(pageNumber, pageSize) {
+    let url = `https://l9r3pr-8080.csb.app/results?_page=${pageNumber}&_limit=${pageSize}`;
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data;
+}
+
+const getLastUserEle = () =>
+    document.querySelector(".user-container > .user:last-child");
+
+const loadUsers = (pageNumber, pageSize) => {
+    return new Promise((resolve, reject) => {
+        toggleLoading(true);
+        getRandomUser(pageNumber, pageSize)
+            .then((data) => {
+                if (data.length === 0) {
+                    resolve("No more data");
+                    return;
+                }
+                data.forEach((user) => renderUser(user));
+                if (isPageLoad) {
+                    observerLastUser();
+                    isPageLoad = false;
+                }
+                toggleLoading(false);
+                resolve("Completed Rendering");
+            })
+            .catch((error) => {
+                toggleLoading(false);
+                reject(error);
+            });
+    });
+};
+
+toggleLoading(true);
+loadUsers(pageNumber, pageSize)
+    .then((data) => {
+        toggleLoading(false);
+    })
+    .catch((error) => {
+        toggleLoading(false);
+    });
+
+const infScrollCallback = (entries, observer) => {
+    const entry = entries[0];
+    if (!entry.isIntersecting) return;
+    pageNumber += 1;
+    toggleLoading(true);
+    loadUsers(pageNumber, pageSize)
+        .then((resp) => {
+            if (resp === "No more data") {
+                observer.unobserve(entry.target);
+                toggleLoading(false);
+                return;
+            }
+            observerLastUser();
+            toggleLoading(false);
+        })
+        .catch((error) => toggleLoading(false));
+    observer.unobserve(entry.target);
+};
+
+const infScrollObserver = new IntersectionObserver(infScrollCallback, {});
+
+const observerLastUser = () => {
+    infScrollObserver.observe(getLastUserEle());
+};
