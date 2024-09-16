8a13ebd9780058f2e008a8a8d53b9c9c5187fdd3
PTT-FULLSTACK <phamtrungtuyentm@gmail.com>
2024-09-17 00:05:32 +0700

    Fix Vercel upload error

--- DAY-44/src/App.jsx
+++ DAY-44/src/App.jsx
@@ -2,13 +2,13 @@ import ButtonFixed from "./components/ButtonFixed/ButtonFixed";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import NavBar from "./components/Header/NavBar";
-import SectionClient from "./components/sections/SectionClient/SectionClient";
+import SectionClient from "./components/Sections/SectionClient/SectionClient";
 import SectionContact from "./components/Sections/SectionContact/SectionContact";
 import SectionMobile from "./components/Sections/SectionMobile/SectionMobile";
 import SectionPopular from "./components/Sections/SectionPopular/SectionPopular";
 import SectionProject from "./components/sections/SectionProject/SectionProject";
 import SectionResume from "./components/Sections/SectionResume/SectionResume";
-import SectionSkill from "./components/sections/SectionSkill/SectionSkill";
+import SectionSkill from "./components/Sections/SectionSkill/SectionSkill";
 
 function App() {
     return (
--- DAY-44/src/components/Footer/Footer.jsx
+++ DAY-44/src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
     return (
-        <footer class="mt-10 px-8 pt-20">
-            <div class="container mx-auto">
-                <div class="mt-16 flex flex-wrap items-center justify-center gap-y-4 border-t border-gray-200 py-6 md:justify-between">
-                    <p class="block antialiased font-sans text-base leading-relaxed text-inherit text-center font-normal !text-gray-700">
+        <footer className="mt-10 px-8 pt-20">
+            <div className="container mx-auto">
+                <div className="mt-16 flex flex-wrap items-center justify-center gap-y-4 border-t border-gray-200 py-6 md:justify-between">
+                    <p className="block antialiased font-sans text-base leading-relaxed text-inherit text-center font-normal !text-gray-700">
                         © 2024 Made with{" "}
                         <a
                             href="https://www.material-tailwind.com"
@@ -17,11 +17,11 @@ function Footer() {
                         </a>
                         .
                     </p>
-                    <ul class="flex gap-8 items-center">
+                    <ul className="flex gap-8 items-center">
                         <li>
                             <a
                                 href="#"
-                                class="block antialiased font-sans text-sm leading-normal font-normal text-gray-700 hover:text-gray-900 transition-colors"
+                                className="block antialiased font-sans text-sm leading-normal font-normal text-gray-700 hover:text-gray-900 transition-colors"
                             >
                                 Home
                             </a>
@@ -29,7 +29,7 @@ function Footer() {
                         <li>
                             <a
                                 href="#"
-                                class="block antialiased font-sans text-sm leading-normal font-normal text-gray-700 hover:text-gray-900 transition-colors"
+                                className="block antialiased font-sans text-sm leading-normal font-normal text-gray-700 hover:text-gray-900 transition-colors"
                             >
                                 About Us
                             </a>
@@ -37,7 +37,7 @@ function Footer() {
                         <li>
                             <a
                                 href="#"
-                                class="block antialiased font-sans text-sm leading-normal font-normal text-gray-700 hover:text-gray-900 transition-colors"
+                                className="block antialiased font-sans text-sm leading-normal font-normal text-gray-700 hover:text-gray-900 transition-colors"
                             >
                                 Blog
                             </a>
@@ -45,13 +45,13 @@ function Footer() {
                         <li>
                             <a
                                 href="#"
-                                class="block antialiased font-sans text-sm leading-normal font-normal text-gray-700 hover:text-gray-900 transition-colors"
+                                className="block antialiased font-sans text-sm leading-normal font-normal text-gray-700 hover:text-gray-900 transition-colors"
                             >
                                 Service
                             </a>
                         </li>
                         <button
-                            class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+                            className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
                             type="button"
                         >
                             subscribe
